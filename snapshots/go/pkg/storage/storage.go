d7ab2897bf5b5936393225b1978e1e4df316a1c8
Taylan Dogan <taylan.dogan@cognite.com>
2022-03-02 13:47:17 +0100

    feat: add storage pkg
    Signed-off-by: Taylan Dogan <taylan.dogan@cognite.com>

---
+++ snapshots/go/pkg/storage/storage.go
@@ -0,0 +1,13 @@
+/* Copyright 2022 Cognite AS */
+
+package storage
+
+import (
+	_ "go.beyondstorage.io/services/gcs/v3"
+	"go.beyondstorage.io/v5/services"
+	"go.beyondstorage.io/v5/types"
+)
+
+func NewStorage(storageUrl string) (types.Storager, error) {
+	return services.NewStoragerFromString(storageUrl)
+}
