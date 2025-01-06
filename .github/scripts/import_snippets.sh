f5c2426dcc5ace33e29a95de45a9871fb0fc0237
Morten Mjelva <morten.mjelva@cognite.com>
2025-01-07 00:10:31 +0100

    Update import snippet

--- .github/scripts/import_snippets.sh
+++ .github/scripts/import_snippets.sh
@@ -15,10 +15,6 @@ VERSION=${TAG//v/}
 SHA=$(shasum -a 256 $TARFILE | awk '{print $1}')
 
 cat << EOF
-## Using Bzlmod
-
-**NOTE: bzlmod support is still beta. APIs subject to change.**
-
 Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
@@ -30,26 +26,5 @@ archive_override(
     urls = ["https://github.com/cognitedata/bazel-snapshots/releases/download/${TAG}/snapshots-${TAG}.tar"],
 )
 
-\`\`\`
-
-## Using WORKSPACE
-
-Paste this snippet into your \`WORKSPACE\` file:
-
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "com_cognitedata_bazel_snapshots",
-    sha256 = "${SHA}",
-    url = "https://github.com/cognitedata/bazel-snapshots/releases/download/${TAG}/snapshots-${TAG}.tar",
-)
-
-load("@com_cognitedata_bazel_snapshots//snapshots:dependencies.bzl", "snapshots_dependencies")
-
-snapshots_dependencies()
-
-load("@com_cognitedata_bazel_snapshots//snapshots:repositories.bzl", "snapshots_register_toolchains")
-
-snapshots_register_toolchains()
 \`\`\`
 EOF
